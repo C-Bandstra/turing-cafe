a537b3f489737e8b6fafb7bd01f09b8cb9f27cca
Charlie Bandstra <bandstracharlie@gmail.com>
2020-05-21 10:59:37 -0600

    Add form class component

---
+++ src/Form/Form.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+// import Form from '../Form/Form'
+// import './Form.css';
+
+class Form extends Component {
+    constructor(props) {
+      super(props)
+      this.state = {
+        date: '',
+        id: null,
+        name: '',
+        number: 0,
+        time: ''
+      }
+    }
+
+    trackInput = (e) => {
+      this.setState({[e.target.id]: e.target.value})
+    }
+
+    handleReservation = (event) => {
+      event.preventDefault()
+      this.props.bookReservation(this.state)
+    }
+
+  render() {
+    return (
+    <section className="form-container">
+      <form onSubmit={this.handleReservation}>
+        <input required id="name" onChange={this.trackInput} className="name-input" placeholder="Name"></input>
+        <input required id="date" onChange={this.trackInput} className="date-input" placeholder="Date"></input>
+        <input required id="time" onChange={this.trackInput} className="time-input" placeholder="Time"></input>
+        <button>Book!</button>
+      </form>
+    </section>  
+    ) 
+  }
+}
+
+export default Form
