b3c941cd6a9b331f7387f57f5509b1f696289696
Charlie Bandstra <bandstracharlie@gmail.com>
2020-05-21 10:59:29 -0600

    add key to component

--- src/CardContainer/CardContainer.js
+++ src/CardContainer/CardContainer.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import Card from '../Card/Card'
+import Card from '../Card/Card';
 import './CardContainer.css';
 
 const CardContainer = ({ reservations }) => {
   const currentReservations = reservations.map(resy => {
-    return <Card {...resy} />
+    return <Card key={resy.id} {...resy} />
   })
-  
+
   return (
-    <section>
+    <section className="card-container">
       {currentReservations}
     </section>
   )
