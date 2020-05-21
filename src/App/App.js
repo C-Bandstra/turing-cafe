5830ac398c5886cdb75d9dacd6e5a1a9568f882f
Charlie Bandstra <bandstracharlie@gmail.com>
2020-05-21 10:59:11 -0600

    ad=d book reservation method

--- src/App/App.js
+++ src/App/App.js
@@ -21,12 +21,18 @@ class App extends Component {
     this.setState({reservations: await fetchReservations()})
   }
 
+  bookReservation = (newReservation) => {
+    const newId = this.state.reservations.length + 1
+    newReservation.id = newId
+    this.setState({reservations: [...this.state.reservations, newReservation]})
+  }
+
   render() {
     return (
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
         <div className="main-container">
-          <Form />
+          <Form bookReservation={this.bookReservation}/>
           <CardContainer reservations={this.state.reservations} />
         </div>
       </div>
