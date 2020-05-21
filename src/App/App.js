cf5800f9c2bd2bf9e400a4c173e324cebcb5bf7f
Charlie Bandstra <bandstracharlie@gmail.com>
2020-05-21 09:54:56 -0600

    Add state, getReservations method, createContainer

--- src/App/App.js
+++ src/App/App.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
+import Card from '../Card/Card'
+import CardContainer from '../CardContainer/CardContainer'
+import { fetchReservations } from '../apiCalls'
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      reservations: []
+    }
+  }
+
+  componentDidMount() {
+    this.getReservations()
+  }
+
+  getReservations = async () => {
+    this.setState({reservations: await fetchReservations()})
+  }
+
   render() {
     return (
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
-        <div className='resy-form'>
-
-        </div>
-        <div className='resy-container'>
-          
-        </div>
+        <CardContainer reservations={this.state.reservations} />
       </div>
     )
   }
